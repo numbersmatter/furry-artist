4473387ac0b55bdc2354d44838da012f0e34a75f
MarkMutt <lleasy8@gmail.com>
2023-04-12 11:36:20 -0400

    added image upload for banner and closed

--- app/routes/site.profile.tsx
+++ app/routes/site.profile.tsx
@@ -1,13 +1,12 @@
-import { Bars3Icon, PhotoIcon, UserCircleIcon } from "@heroicons/react/20/solid";
-import { ActionArgs, LoaderArgs, unstable_composeUploadHandlers, unstable_createMemoryUploadHandler, unstable_parseMultipartFormData, UploadHandler } from "@remix-run/node";
+import { Bars3Icon, UserCircleIcon } from "@heroicons/react/20/solid";
+import type { ActionArgs, LoaderArgs, UploadHandler } from "@remix-run/node";
+import { unstable_composeUploadHandlers, unstable_createMemoryUploadHandler, unstable_parseMultipartFormData } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
-import { Form, useActionData, useLoaderData, useNavigation, useSubmit, useTransition } from "@remix-run/react";
+import { Form, useActionData, useLoaderData, useNavigation, useSubmit, } from "@remix-run/react";
 import { useEffect, useRef, useState } from "react";
 import { uploadImage } from "~/server/cloudinary.server";
 import { getProfilePageHeaderDoc, setProfilePageHeaderDoc } from "~/server/database/profile.server";
 import { baseLoader } from "~/server/user.server";
-import SectionPanel from "~/ui/Layout/SectionPanel";
-import StackedField from "~/ui/StackedFields/StackFields";
 
 export async function action({ params, request }: ActionArgs) {
   const {profileId, userRecord} = await baseLoader(request);
@@ -43,7 +42,6 @@ export async function action({ params, request }: ActionArgs) {
     return json({ error: "something wrong" });
   }
   // random id
-  const imageId = Math.random().toString(36).substring(2, 15) + Math.random().toString(36).substring(2, 15);
 
   await setProfilePageHeaderDoc({
     profileId,
@@ -200,21 +198,8 @@ export default function ProfileData() {
                     </button>
                   </div>
                 </div>
-
-
               </div>
             </div>
-            {/* <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-              <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save
-              </button>
-            </div> */}
           </Form>
         </div>
         <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
@@ -242,7 +227,7 @@ export default function ProfileData() {
                     {
                       pageHeaderData?.bannerImage ? (
                         <img
-                          className="h-12 w-12 rounded-full"
+                          className="h-12 w-12"
                           src={pageHeaderData?.bannerImage}
                           alt=""
                         />
@@ -292,23 +277,160 @@ export default function ProfileData() {
 
               </div>
             </div>
-            {/* <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
-              <button type="button" className="text-sm font-semibold leading-6 text-gray-900">
-                Cancel
-              </button>
-              <button
-                type="submit"
-                className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-              >
-                Save
-              </button>
-            </div> */}
           </Form>
         </div>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
+          <div className="px-4 sm:px-0">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">Profile Homepage Image</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              This will be the image used for your profile homepage.
+            </p>
+          </div>
+          <Form
+            replace
+            method="POST"
+            // @ts-ignore
+            ref={formRef}
+            encType="multipart/form-data"
+            className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2"
+          >
+            <div className="px-4 py-6 sm:p-8">
+              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="col-span-full">
+                  <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                    Photo
+                  </label>
+                  <div className="mt-2 flex items-center gap-x-3">
+                    {
+                      pageHeaderData?.heroImage ? (
+                        <img
+                          className="h-12 w-12 "
+                          src={pageHeaderData?.heroImage}
+                          alt=""
+                        />
+                      ) : (
+                        <Bars3Icon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                      )
+                    }
+                    <input
+                      ref={fileInputRef}
+                      hidden
+                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                      onChange={(e) => checkFilesPresent(e)}
+                      id="img-field"
+                      type="file"
+                      name="img"
+                      accept="image/*"
+                    />
+                    <input hidden name="fieldName" value="heroImage" readOnly />
+                    <button
+                    type="button"
+                      // className={isUploading ? disabledClass : regularClass}
+                      onClick={openFileInput}
+                      disabled={isUploading}
+                      >
+                      
+                      {isUploading ? "Uploading..." : "Upload Image"}
+                    </button>
+
+                    <input
+                      className="hidden"
+                      name="_action"
+                      value="uploadImage"
+                      readOnly
+                      />
+
+                    <button
+                      onClick={openFileInput}
+                      disabled={isUploading}
+                      type="button"
+                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    >
+                      Change
+                    </button>
+                  </div>
+                </div>
 
 
+              </div>
+            </div>
+          </Form>
+        </div>
+        <div className="grid grid-cols-1 gap-x-8 gap-y-8 md:grid-cols-3">
+          <div className="px-4 sm:px-0">
+            <h2 className="text-base font-semibold leading-7 text-gray-900">All forms closed</h2>
+            <p className="mt-1 text-sm leading-6 text-gray-600">
+              This will be the image used when all forms are closed.
+            </p>
+          </div>
+          <Form
+            replace
+            method="POST"
+            // @ts-ignore
+            ref={formRef}
+            encType="multipart/form-data"
+            className="bg-white shadow-sm ring-1 ring-gray-900/5 sm:rounded-xl md:col-span-2"
+          >
+            <div className="px-4 py-6 sm:p-8">
+              <div className="grid max-w-2xl grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+                <div className="col-span-full">
+                  <label htmlFor="photo" className="block text-sm font-medium leading-6 text-gray-900">
+                    Photo
+                  </label>
+                  <div className="mt-2 flex items-center gap-x-3">
+                    {
+                      pageHeaderData?.closedImage ? (
+                        <img
+                          className="h-12 w-12 "
+                          src={pageHeaderData?.heroImage}
+                          alt=""
+                        />
+                      ) : (
+                        <Bars3Icon className="h-12 w-12 text-gray-300" aria-hidden="true" />
+                      )
+                    }
+                    <input
+                      ref={fileInputRef}
+                      hidden
+                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                      onChange={(e) => checkFilesPresent(e)}
+                      id="img-field"
+                      type="file"
+                      name="img"
+                      accept="image/*"
+                    />
+                    <input hidden name="fieldName" value="closedImage" readOnly />
+                    <button
+                    type="button"
+                      // className={isUploading ? disabledClass : regularClass}
+                      onClick={openFileInput}
+                      disabled={isUploading}
+                      >
+                      
+                      {isUploading ? "Uploading..." : "Upload Image"}
+                    </button>
 
+                    <input
+                      className="hidden"
+                      name="_action"
+                      value="uploadImage"
+                      readOnly
+                      />
 
+                    <button
+                      onClick={openFileInput}
+                      disabled={isUploading}
+                      type="button"
+                      className="rounded-md bg-white px-2.5 py-1.5 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
+                    >
+                      Change
+                    </button>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Form>
+        </div>
       </div>
     </div>
   );

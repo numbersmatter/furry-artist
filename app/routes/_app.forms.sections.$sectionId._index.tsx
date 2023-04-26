07363c50654d6270de48bef4827531bbaec7f7c5
MarkMutt <lleasy8@gmail.com>
2023-04-26 10:17:44 -0400

    Create edit basic info form

--- app/routes/_app.forms.sections.$sectionId._index.tsx
+++ app/routes/_app.forms.sections.$sectionId._index.tsx
@@ -2,12 +2,13 @@ import { ArrowDownIcon, ArrowUpIcon } from "@heroicons/react/24/outline";
 import type { ActionArgs, LoaderArgs } from "@remix-run/node";
 import { json, redirect } from "@remix-run/node";
 import { Form, Link, useActionData, useLoaderData } from "@remix-run/react";
-import type { ReactNode } from "react";
+import { ReactNode, useState } from "react";
 import { requireAuth } from "~/server/auth.server";
 import { getUserDoc } from "~/server/database/db.server";
 import { addField, getFormSectionById, moveArrayElement, updateSectionDoc } from "~/server/database/forms.server";
 import type { Field } from "~/ui/StackedFields/StackFields";
 import * as z from "zod";
+import { PencilIcon } from "@heroicons/react/20/solid";
 
 export async function action({ params, request }: ActionArgs) {
   const userRecord = await requireAuth(request);
@@ -47,7 +48,7 @@ export async function action({ params, request }: ActionArgs) {
         sectionId: params.sectionId,
         field: fieldData
       })
-      if( modFields && fieldData.type === "select"){
+      if (modFields && fieldData.type === "select") {
         return redirect(`fields/${modFields.fieldId}`)
       }
 
@@ -130,6 +131,7 @@ export async function loader({ params, request }: LoaderArgs) {
 export default function EditFormSection() {
   const { formSectionDoc, sectionFields, saveUrl } = useLoaderData<typeof loader>();
   const actionData = useActionData();
+  const [editMode, setEditMode] = useState(false);
 
   const fields = formSectionDoc.fieldOrder.map((fieldId) => {
     return formSectionDoc.fieldData[fieldId]
@@ -138,11 +140,15 @@ export default function EditFormSection() {
   return (
     <div className="px-0 py-0 sm:py-2 sm:px-4">
       {actionData ? <p>{JSON.stringify(actionData)}</p> : <p></p>}
-      <SectionPanel name={formSectionDoc.name} text={formSectionDoc.text} >
-        {
+      <div className="bg-white  px-4 py-5 shadow sm:rounded-lg sm:p-6">
+      {
+        editMode
+          ? <SectionEditTitleForm edit={setEditMode} name={formSectionDoc.name} text={formSectionDoc.text} />
+          : <SectionBasicInfo edit={setEditMode} name={formSectionDoc.name} text={formSectionDoc.text} />
+      }        {
           formSectionDoc.type === "imageUpload"
             ? <p
-            className="col-span-1 sm:col-span-6"
+              className="col-span-1 sm:col-span-6"
             >Image Upload Form sections can have no fields</p>
             :
             <ul className=" col-span-1 sm:col-span-6">
@@ -159,7 +165,7 @@ export default function EditFormSection() {
             </ul>
         }
 
-      </SectionPanel>
+      </div>
       <div className=" py-4 flex justify-end">
         <Link to={saveUrl}
           className="bg-gray-50 text-gray-900 px-4 py-2 rounded-md shadow-sm text-sm font-medium hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
@@ -236,28 +242,151 @@ function AddField() {
   )
 }
 
-
-
-function SectionPanel(props: { name: string, text: string, children: ReactNode }) {
-
+function SectionBasicInfo({ name, text, edit }: { name: string, text: string, edit: (edit: boolean) => void }) {
   return (
-    <div className="bg-white  px-4 py-5 shadow sm:rounded-lg sm:p-6">
-      <div className="space-y-8 divide-y divide-gray-200">
+    <div className="space-y-8 divide-y divide-gray-200">
         <div>
           <div>
-            <h3 className="text-2xl font-semibold leading-6 text-gray-900">{props.name}</h3>
+            <h3 className="text-2xl font-semibold leading-6 text-gray-900">{name}</h3>
             <p className="mt-1 text-base text-gray-500">
-              {props.text}
+              {text}
             </p>
           </div>
-          <div className="mt-6 grid grid-cols-1 gap-y-6 gap-x-4 sm:grid-cols-6">
-            {props.children}
-          </div>
+          <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+          <button
+            type="button"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+            onClick={() => edit(true)}
+          >
+            Edit
+          </button>
+        </div>
         </div>
       </div>
-    </div>
+  )
+}
+
+function SectionEditTitleForm(props: { name: string, text: string, edit: (edit: boolean) => void }) {
+
+  return (
+      <div className="space-y-8 divide-y divide-gray-200">
+        <div className="mt-10 space-y-8 border-b border-gray-900/10 pb-12 sm:space-y-0 sm:divide-y sm:divide-gray-900/10 sm:border-t sm:pb-0">
+          <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+            <label htmlFor="username" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+              Section Title
+            </label>
+            <div className="mt-2 sm:col-span-2 sm:mt-0">
+              <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-gray-300 focus-within:ring-2 focus-within:ring-inset focus-within:ring-indigo-600 sm:max-w-md">
+                <input
+                  type="text"
+                  name="username"
+                  id="username"
+                  autoComplete="username"
+                  className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                  placeholder="janesmith"
+                  defaultValue={props.name}
+                />
+              </div>
+            </div>
+          </div>
+
+          <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
+            <label htmlFor="about" className="block text-sm font-medium leading-6 text-gray-900 sm:pt-1.5">
+              Section Description
+            </label>
+            <div className="mt-2 sm:col-span-2 sm:mt-0">
+              <textarea
+                id="sectionDescription"
+                name="sectionDescription"
+                rows={3}
+                className="block w-full max-w-2xl rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                defaultValue={props.text}
+              />
+              <p className="mt-3 text-sm leading-6 text-gray-600">
+                Brief description for your section. This is shown to users on the form.
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="flex items-center justify-end gap-x-6 border-t border-gray-900/10 px-4 py-4 sm:px-8">
+          <button 
+            type="button" 
+            className="text-sm font-semibold leading-6 text-gray-900"
+            onClick={() => props.edit(false)}
+          >
+            Cancel
+          </button>
+          <button
+            type="submit"
+            className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
+            Save
+          </button>
+        </div>
+
+      </div>
   );
 };
+
+/*
+  This example requires some changes to your config:
+  
+  ```
+  // tailwind.config.js
+  module.exports = {
+    // ...
+    plugins: [
+      // ...
+      require('@tailwindcss/forms'),
+    ],
+  }
+  ```
+*/
+
+
+function ExampleTextArea() {
+
+  return (
+    <div className="flex items-start space-x-4">
+      <div className="flex-shrink-0">
+        <img
+          className="inline-block h-10 w-10 rounded-full"
+          src="https://images.unsplash.com/photo-1550525811-e5869dd03032?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          alt=""
+        />
+      </div>
+      <div className="min-w-0 flex-1">
+        <form action="#">
+          <div className="border-b border-gray-200 focus-within:border-indigo-600">
+            <label htmlFor="comment" className="sr-only">
+              Add your comment
+            </label>
+            <textarea
+              rows={3}
+              name="comment"
+              id="comment"
+              className="block w-full resize-none border-0 border-b border-transparent p-0 pb-2 text-gray-900 placeholder:text-gray-400 focus:border-indigo-600 focus:ring-0 sm:text-sm sm:leading-6"
+              placeholder="Add your comment..."
+              defaultValue={''}
+            />
+          </div>
+          <div className="flex justify-between pt-2">
+
+            <div className="flex-shrink-0">
+              <button
+                type="submit"
+                className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              >
+                Post
+              </button>
+            </div>
+          </div>
+        </form>
+      </div>
+    </div>
+  )
+}
+
 
 
 function FieldCard(props: {
